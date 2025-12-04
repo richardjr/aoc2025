df8b2fc3a84c63533df6fc31f0847d1650bca37c
Richard Reynolds <xrouter@gmail.com>
2025-12-04 10:48:28 +0000

    added timer

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import time
+
 from libs.loaders import get_file_with_mode, cmd_line_arguments
 from aocd import submit
 
@@ -10,7 +12,11 @@ if __name__ == "__main__":
 
     parsed = day_module.parse(input_data)
     module_name = f"part_{part}"
+    # time to call the function
+    start_time = time.time()
     func = getattr(day_module, module_name)
     result = func(parsed)
+    end_time = time.time()
+    print(f"Execution Time for Day {day} Part {part}: {end_time - start_time} seconds\n")
     if sub == True and mode == 'live':
         submit(result, part=part, day=day, year=2025)
